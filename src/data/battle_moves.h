51ddb2c09208e154e62861a35320fa73026acd01
Valmerk30 <valmerk30@gmail.com>
2025-03-17 19:46:09 +0100

    Update battle_moves.h

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -5068,7 +5068,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_SHEER_FORCE_BOOST,
-        .split = SPLIT_SPECIAL,
+        .split = SPLIT_HIGHEST,
     },
 
     [MOVE_SKY_UPPERCUT] =
