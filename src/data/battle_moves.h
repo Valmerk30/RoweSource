f8702048c397a7449d7aee41b90f0cf12c5c6703
Valmerk30 <valmerk30@gmail.com>
2025-06-30 17:28:45 +0200

    Update battle_moves.h

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -9729,7 +9729,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .target = MOVE_TARGET_RANDOM,
         .priority = 0,
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED,
-        .split = SPLIT_PHYSICAL,
+        .split = SPLIT_SPECIAL,
     },
 	
     [MOVE_DRAGON_HAMMER] =
